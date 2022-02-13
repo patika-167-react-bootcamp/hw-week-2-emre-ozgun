f27a50293595728cdf3a6b3cb8a92fb332556a8e
emre-ozgun <emre2996ozgun@gmail.com>
2022-02-13 20:00:11 +0300

    finalize core functionality

--- app.js
+++ app.js
@@ -1,39 +1,58 @@
 const globalState = {
 	accountState: [],
 	historyState: [],
-	// loading: {type: '', flag: false}
 };
 
-//loading state
-// let isLoading = false;
-
-//local state
+//local account state
 let accountInfo = {
 	name: '',
 	balance: 0,
+	id: '',
 };
 
-//local state
-//intermediary -> history -> account - use and dump
-//search -> find -> update (from--, to++) - this search operation is mediocre (should-ve been done via email - multiple accounts might share the same name.)
-// let toBeUpdated = {
-//   to_ID : '',
-//   from_ID : ''
-// }
+//local transaction state
+let transactionInfo = {
+	from: { id: '', name: '' },
+	to: { id: '', name: '' },
+	amount: 0,
+};
+
+//local history state
+let transactionHistory = '';
 
 const setState = (type, payload) => {
 	if (type === 'ADD_ACCOUNT') {
 		globalState.accountState = [...globalState.accountState, payload];
 		triggerAccountRender();
+		updateTransactionSearch();
 	}
 
 	if (type === 'ADD_TRANSACTION') {
-		// triggerHistoryRender();
-		// triggerAccountRender();
+		updateBalance(payload);
+		updateHistory(payload);
+		triggerAccountRender();
+		triggerHistoryRender();
 	}
+};
+
+const updateBalance = (transactionInfo) => {
+	globalState.accountState = globalState.accountState.map((account) => {
+		if (account.id === transactionInfo.from.id) {
+			return { ...account, balance: account.balance - transactionInfo.amount };
+		}
+		if (account.id === transactionInfo.to.id) {
+			return { ...account, balance: account.balance + transactionInfo.amount };
+		}
+		return account;
+	});
 
 	console.log(globalState);
-	triggerRender();
+};
+
+const updateHistory = (transactionInfo) => {
+	transactionHistory = `${transactionInfo.from.name} sent $${transactionInfo.amount} to ${transactionInfo.to.name}`;
+	globalState.historyState = [...globalState.historyState, transactionHistory];
+	transactionHistory = '';
 };
 
 const triggerAccountRender = () => {
@@ -43,7 +62,10 @@ const triggerAccountRender = () => {
 	const generateAccountDOM = (id, name, balance) => {
 		const li = document.createElement('li');
 		li.dataset.id = id;
-		li.innerHTML = `<span id="acc-name">${name}</span><span class="acc-balance">$${balance}</span>`;
+		li.innerHTML = `
+    <span id="acc-name">${name}</span>
+    <span class="acc-balance">$${balance}</span>
+    `;
 
 		return li;
 	};
@@ -53,8 +75,16 @@ const triggerAccountRender = () => {
 	);
 };
 
-const triggerRender = () => {
-	const transactionList = document.querySelector('transaction-list');
+const triggerHistoryRender = () => {
+	const transactionHistoryList = document.querySelector('.transaction-list');
+	transactionHistoryList.innerHTML = '';
+
+	globalState.historyState.forEach((history) => {
+		let historyLi = document.createElement('li');
+		historyLi.classList.add = 'transaction-list-item';
+		historyLi.innerText = history;
+		transactionHistoryList.append(historyLi);
+	});
 };
 
 const accountForm = document.querySelector('.add-user');
@@ -110,12 +140,176 @@ const handleAccountChange = (e) => {
 		addAccountBtn.disabled = true;
 	}
 
-	console.log(accountInfo);
+	// console.log(accountInfo);
 };
 
 accountForm.addEventListener('submit', handleAccountSubmit);
 accountForm.addEventListener('keyup', handleAccountChange);
 
-//populate fields from LS
-// const getDataFromLS = () =>
-//  window.addEventListener('DOMContentLoaded', getDataFromLS)
+// TRANSACTIONS
+
+const transactionForm = document.querySelector('.add-transaction');
+
+const searchAccount = transactionForm.querySelector('.search-account');
+
+const clearTransactionForm = () => {
+	transactionInfo = {
+		from: { id: '', name: '' },
+		to: { id: '', name: '' },
+		amount: 0,
+	};
+	document.querySelector('#transaction-submit').disabled = true;
+	document.querySelector('#transaction-amount').style.display = 'none';
+	document.querySelector('#transaction-amount').value = '';
+	document.querySelector('#transaction-results').style.display = 'none';
+	updateTransactionInfo();
+	shouldAmountBeDisplayed();
+};
+
+const handleTransactionSubmit = () => {
+	setState('ADD_TRANSACTION', transactionInfo);
+	clearTransactionForm();
+};
+
+const fetchSearchResults = () => {
+	let li = '';
+	if (globalState.accountState.length < 1) {
+		li = `<li>Your account list is currently empty, consider adding an account.</li>`;
+	} else {
+		globalState.accountState.forEach(
+			({ id, name }) =>
+				(li += `
+			<li data-id="${id}">${name}</li>`)
+		);
+	}
+
+	return li;
+};
+
+const resultFrom = document.querySelector('#result-from');
+const resultTo = document.querySelector('#result-to');
+
+const updateTransactionInfo = () => {
+	resultFrom.innerText = transactionInfo.from.name;
+	resultTo.innerText = transactionInfo.to.name;
+	transactionForm.querySelector('.search-account').style.display = 'none';
+	document.querySelector('.search-results').classList = 'search-results';
+
+	console.log(transactionInfo);
+};
+
+const shouldAmountBeDisplayed = () => {
+	if (transactionInfo.from.name && transactionInfo.to.name) {
+		const maxAmount = globalState.accountState.find(
+			(user) => user.id === transactionInfo.from.id
+		).balance;
+		document.querySelector('#transaction-amount').style.display = 'block';
+		document.querySelector(
+			'#transaction-amount'
+		).placeholder = `Amount [max $(${maxAmount})]`;
+	} else {
+		document.querySelector('#transaction-amount').style.display = 'none';
+	}
+};
+
+const makeSelection = (e) => {
+	if (e.currentTarget.classList.contains('from')) {
+		if (e.target.dataset.id) {
+			transactionInfo.from = {
+				id: e.target.dataset.id,
+				name: e.target.textContent,
+			};
+		}
+	}
+	if (e.currentTarget.classList.contains('to')) {
+		if (e.target.dataset.id) {
+			transactionInfo.to = {
+				id: e.target.dataset.id,
+				name: e.target.textContent,
+			};
+		}
+	}
+
+	if (transactionInfo.from.name || transactionInfo.to.name) {
+		document.querySelector('#transaction-results').style.display = 'flex';
+	}
+
+	console.log(transactionInfo);
+
+	updateTransactionInfo();
+	shouldAmountBeDisplayed();
+};
+
+const listenTransactionSelection = () => {
+	const transactionResults = document.querySelector('.search-results');
+	transactionResults.addEventListener('click', makeSelection);
+};
+
+const updateTransactionSearch = () => {
+	transactionForm.querySelector('.search-results').innerHTML =
+		fetchSearchResults();
+};
+
+const displaySearchResults = (identifier) => {
+	let searchIdentifier = document.querySelector('#search-input');
+
+	if (identifier === 'from') {
+		searchIdentifier.placeholder = 'SENDER: Search Account';
+		document.querySelector('.search-results').classList.add('from');
+	} else {
+		searchIdentifier.placeholder = 'RECEIVER: Search Account';
+		document.querySelector('.search-results').classList.add('to');
+	}
+
+	updateTransactionSearch();
+	transactionForm.querySelector('.search-account').style.display = 'block';
+	listenTransactionSelection();
+};
+
+const locateTransactionEvent = (e) => {
+	if (e.target.id === 'from-btn') {
+		displaySearchResults('from');
+	}
+
+	if (e.target.id === 'to-btn') {
+		displaySearchResults('to');
+	}
+
+	if (e.target.id === 'close-search') {
+		transactionForm.querySelector('.search-account').style.display = 'none';
+		document.querySelector('.search-results').classList = 'search-results';
+	}
+
+	if (e.target.id === 'transaction-submit') {
+		e.preventDefault();
+		handleTransactionSubmit();
+	}
+};
+
+transactionForm.addEventListener('click', locateTransactionEvent);
+transactionForm
+	.querySelector('#transaction-amount')
+	.addEventListener('keyup', (e) => {
+		const maxBalance = globalState.accountState.find(
+			(user) => user.id === transactionInfo.from.id
+		).balance;
+
+		if (+e.target.value > maxBalance) {
+			e.target.value = maxBalance;
+		}
+		if (
+			+e.target.value < 0 ||
+			e.target.value.startsWith('-') ||
+			e.target.value.startsWith('00')
+		) {
+			e.target.value = '';
+			return;
+		}
+		transactionInfo.amount = +e.target.value;
+
+		if (transactionInfo.amount > 0 && transactionInfo.amount <= maxBalance) {
+			document.querySelector('#transaction-submit').disabled = false;
+		} else {
+			document.querySelector('#transaction-submit').disabled = true;
+		}
+	});
